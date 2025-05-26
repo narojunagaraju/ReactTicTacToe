c6ac367e0986ae112777f3e58e5bf8c92c818a72
naroju nagaraju <narojunagarajun@gmail.com>
2025-05-26 15:47:35 +0530

    designed game board

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react"
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+      const [playerName,setPlayerName] = useState(initialName);
       const [isEditing, setIsEdititng] = useState(false);
 
       function handleEditClick() {
-            setIsEdititng(true);
+            //When updating state based on prev state always use function instead of setIsEdititng(!isEditing); 
+            // bcz react cannot update state instantly instead it schedules...
+            setIsEdititng((editing) => !editing);
       }
-      let playerName = <span className="player-name">{name}</span>
+
+      function handleChange(event) {
+            setPlayerName(event.target.value)
+      }
+
+
+      let editablePlayerName = <span className="player-name">{playerName}</span>
       if (isEditing) {
-            playerName = <input type="text" required />
+            editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
       }
       return (
             <li>
                   <span className="player">
-                        {playerName}
+                        {editablePlayerName}
                         <span className="player-symbol">{symbol}</span>
                   </span>
-                  <button onClick={handleEditClick}>Edit</button>
+                  <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
             </li>
       );
 }
